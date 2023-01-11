de1c93e9da831f73ea45c608daa6062fb3a0ddcc
Hansen Halim <fpsecond.hh@gmail.com>
2023-01-11 08:06:59 +0700

    Refresh UI

--- resources/js/Pages/Photo/Create.vue
+++ resources/js/Pages/Photo/Create.vue
@@ -3,61 +3,62 @@
         <div
             class="flex min-h-screen w-screen items-center justify-center text-white"
         >
-            <div class="flex items-center space-x-10">
-                <div class="w-40">
-                    <div
-                        v-show="photoPaper.photos.length > 0"
-                        class="mt-2 animate-pulse bg-zinc-800 py-2 px-3 text-zinc-400"
-                    >
-                        Tip: Click on image to change filter
+            <div class="flex flex-col items-center">
+                <div class="flex space-x-4">
+                    <div class="flex-1">
+                        <div class="w-52 rounded-xl border px-4 py-2 text-lg">
+                            Tip: Click on image to change filter
+                        </div>
+                    </div>
+                    <video class="flex-1" id="webcam" muted autoplay></video>
+                    <div class="flex w-52 flex-1 flex-col space-y-4">
+                        <div
+                            v-for="photo in photoPaper.photos"
+                            :key="photo.id"
+                            class="flex w-56 space-x-2"
+                        >
+                            <Link
+                                method="put"
+                                as="button"
+                                :href="route('photos.update', photo)"
+                            >
+                                <img :src="photo.final_url" />
+                            </Link>
+                            <Link
+                                :href="route('photos.destroy', photo)"
+                                method="delete"
+                                as="button"
+                            >
+                                <XCircleIcon class="h-14 w-14"
+                            /></Link>
+                        </div>
                     </div>
                 </div>
-
-                <div class="flex w-28 flex-col items-center">
-                    <video id="webcam" muted autoplay></video>
-                    <div v-if="captureLeft > 0">
-                        <div>Capture left: {{ captureLeft }}</div>
+                <div class="mt-16 grid h-12 grid-cols-3">
+                    <div></div>
+                    <div class="relative flex items-center justify-center">
+                        <div
+                            v-if="captureLeft > 0"
+                            class="absolute m-2 h-16 w-16 animate-ping rounded-full bg-red-500"
+                        ></div>
                         <button
+                            :disabled="!(captureLeft > 0)"
                             @click="sendCaptureCommand"
-                            class="mt-2 w-full border p-2"
+                            class="absolute rounded-full border-4 bg-red-500 p-4 hover:bg-red-300 active:bg-red-300 disabled:bg-gray-500"
                         >
-                            Capture
+                            <CameraIcon class="h-10 w-10" />
                         </button>
                     </div>
-                    <div v-else>
-                        <div>Ready to print</div>
-                        <Link
-                            :href="route('photo-papers.update', photoPaper)"
-                            method="put"
-                            as="button"
-                            class="mt-2 w-full border p-2"
-                        >
-                            Print
-                        </Link>
-                    </div>
-                </div>
-
-                <div class="relative grid w-56 gap-2">
-                    <div
-                        v-for="photo in photoPaper.photos"
-                        :key="photo.id"
-                        class="flex space-x-4"
+                    <Link
+                        v-if="!(captureLeft > 0)"
+                        :href="route('photo-papers.update', photoPaper)"
+                        method="put"
+                        as="button"
+                        class="flex items-center rounded-full border py-2 px-4 text-xl hover:bg-white hover:text-black active:bg-white active:text-black"
                     >
-                        <Link
-                            method="put"
-                            as="button"
-                            :href="route('photos.update', photo)"
-                        >
-                            <img :src="photo.final_url" />
-                        </Link>
-                        <Link
-                            :href="route('photos.destroy', photo)"
-                            method="delete"
-                            as="button"
-                        >
-                            <XCircleIcon class="h-10 w-10"
-                        /></Link>
-                    </div>
+                        Continue
+                        <ArrowRightIcon class="ml-1 h-8 w-8" />
+                    </Link>
                 </div>
             </div>
         </div>
@@ -66,7 +67,11 @@
 
 <script setup>
 import FullscreenLayout from "@/Layouts/FullscreenLayout.vue";
-import { XCircleIcon } from "@heroicons/vue/24/outline";
+import {
+    ArrowRightIcon,
+    CameraIcon,
+    XCircleIcon,
+} from "@heroicons/vue/24/outline";
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/inertia-vue3";
 import { computed, onMounted } from "vue";
