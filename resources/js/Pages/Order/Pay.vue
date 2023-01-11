db542ecc49a43ba71f361397a1ff8e7e54dad91a
Hansen Halim <fpsecond.hh@gmail.com>
2023-01-11 09:18:00 +0700

    finish

--- resources/js/Pages/Order/Pay.vue
+++ resources/js/Pages/Order/Pay.vue
@@ -2,7 +2,53 @@
     <Head title="Checkout" />
 
     <FullscreenLayout>
-        <div class="flex min-h-screen items-center justify-center"></div>
+        <div class="flex min-h-screen items-center justify-center">
+            <div
+                class="fixed mt-10 h-56 w-56"
+                style="
+                    z-index: 1000000;
+                    box-shadow: 0 0 0 99999px rgba(255, 255, 255, 1);
+                "
+            >
+                <div
+                    class="fixed top-0 left-0 flex w-screen flex-col items-center"
+                >
+                    <div
+                        class="border-x-4 border-b-4 border-black px-4 pt-8 pb-2 text-center text-xl font-bold"
+                    >
+                        PITIBI Payment could be done by
+                    </div>
+                    <div
+                        class="absolute -mt-52 grid h-screen grid-flow-col place-items-center gap-4"
+                    >
+                        <div
+                            class="h-20 w-20 bg-contain bg-center"
+                            style="background-image: url('/qris/1.png')"
+                        />
+                        <div
+                            class="h-20 w-20 bg-contain bg-center"
+                            style="background-image: url('/qris/2.png')"
+                        />
+                        <div
+                            class="h-20 w-20 bg-contain bg-center"
+                            style="background-image: url('/qris/3.png')"
+                        />
+                        <div
+                            class="h-20 w-20 rounded-2xl border bg-contain bg-center"
+                            style="background-image: url('/qris/4.png')"
+                        />
+                        <div
+                            class="h-20 w-20 bg-contain bg-center"
+                            style="background-image: url('/qris/5.png')"
+                        />
+                        <div
+                            class="h-20 w-20 bg-contain bg-center"
+                            style="background-image: url('/qris/6.png')"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
     </FullscreenLayout>
 </template>
 
