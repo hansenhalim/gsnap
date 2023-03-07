cbb59cf2efc31ae84574842fa25ea8177f9895bf
V Hansen Halim <fpsecond.hh@gmail.com>
2023-03-07 10:15:17 +0700

    Create countdown before capture

--- resources/js/Pages/Photo/Create.vue
+++ resources/js/Pages/Photo/Create.vue
@@ -5,8 +5,23 @@
         >
             <div class="flex flex-col items-center">
                 <div class="flex w-[1440px] space-x-4">
-                    <video id="webcam" class="w-1/2" muted autoplay />
-                    <div class="flex w-1/2 flex-col justify-center space-y-4">
+                    <div class="relative w-1/2 border">
+                        <video
+                            id="webcam"
+                            class="relative w-full"
+                            muted
+                            autoplay
+                        />
+                        <div
+                            v-if="isTimerOn"
+                            class="absolute top-0 left-0 flex h-full w-full items-center justify-center bg-black/50"
+                        >
+                            <div class="text-9xl">{{ countdown }}</div>
+                        </div>
+                    </div>
+                    <div
+                        class="flex w-1/2 flex-col justify-center space-y-4 border"
+                    >
                         <div
                             v-for="photo in photoPaper.photos"
                             :key="photo.id"
@@ -33,11 +48,11 @@
                     <div></div>
                     <div class="relative flex items-center justify-center">
                         <div
-                            v-if="captureLeft > 0"
+                            v-if="!captureEnabled"
                             class="absolute m-2 h-16 w-16 animate-ping rounded-full bg-red-500"
                         ></div>
                         <button
-                            :disabled="!(captureLeft > 0)"
+                            :disabled="captureEnabled"
                             @click="sendCaptureCommand"
                             class="absolute rounded-full border-4 bg-red-500 p-4 hover:bg-red-300 active:bg-red-300 disabled:bg-gray-500"
                         >
@@ -69,7 +84,9 @@ import {
 } from "@heroicons/vue/24/outline";
 import { Inertia } from "@inertiajs/inertia";
 import { Link } from "@inertiajs/inertia-vue3";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
+
+const timerSeconds = 5; //set this to desired value
 
 const props = defineProps({
     photoPaper: Object,
@@ -78,11 +95,35 @@ const props = defineProps({
     uploadUrl: String,
 });
 
+const isTimerOn = ref(false);
+const countdown = ref(timerSeconds);
+
 const captureLeft = computed(
     () => props.photoPaper.frame.slot_count - props.photoPaper.photos.length
 );
 
+const captureEnabled = computed(
+    () => isTimerOn.value || captureLeft.value <= 0
+);
+
 const sendCaptureCommand = () => {
+    // overlay video with timer
+    isTimerOn.value = true;
+
+    const countdownInterval = setInterval(() => {
+        // start reducing countdown
+        countdown.value--;
+
+        // when countdown reaching 0
+        if (countdown.value <= 0) {
+            //stop interval
+            clearInterval(countdownInterval);
+            // reset timer
+            countdown.value = timerSeconds;
+            isTimerOn.value = false;
+        }
+    }, 1000);
+
     const options = {
         method: "POST",
         headers: {
@@ -96,9 +137,11 @@ const sendCaptureCommand = () => {
         }),
     };
 
-    fetch(props.triggerUrl, options)
-        .then(() => Inertia.reload())
-        .catch((err) => console.error(err));
+    setTimeout(() => {
+        fetch(props.triggerUrl, options)
+            .then(() => Inertia.reload())
+            .catch((err) => console.error(err));
+    }, timerSeconds * 1000);
 };
 
 onMounted(() => {
